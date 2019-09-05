6e8f9c343d6f1816b2c9c2bd12f1aa0dc34d0087
XIAOHUI LIAO <182272546@qq.com>
2019-09-05 15:02:26 +1000

    completed the result page

--- src/containers/QuoteForm/QuoteForm.js
+++ src/containers/QuoteForm/QuoteForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import CurrencyInput from 'react-currency-input';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import './QuoteForm.css';
 
@@ -8,11 +8,11 @@ class QuoteForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currencies: ['AUD', 'EUR', 'USD', 'GBP', 'JPY', 'SGD', 'NZD', 'KRW', 'INR', 'IDR', 'CNY', 'CAD', 'EGP'],
+            currencies: ['AUD', 'EUR', 'GBP', 'JPY', 'USD','SGD', 'NZD', 'KRW', 'INR', 'IDR', 'CNY', 'CAD', 'EGP'],
             fromCurrency: 'AUD',
             rate: '',
             amount: "0.00",
-            toCurrency: '',
+            toCurrency: 'AUD',
             result: ''
         };
     }
@@ -32,118 +32,112 @@ class QuoteForm extends Component {
     handleInputChange = (event) => {
         this.setState({
             amount: event.target.value
-        }, this.getCurrencyData);
+        });
     }
 
-    getCurrencyData = () => {
+    getCurrencyData = (event) => {
+        event.preventDefault();
         const amount = this.state.amount;
+        const fromCurrency = this.state.fromCurrency;
+        const toCurrency = this.state.toCurrency;
         if (amount === isNaN) {
             return
         } else {
-           /* axios.get(`https://api.ofx.com/PublicSite.ApiService/OFX/spotrate/Individual/${this.state.fromCurrency}/${this.state.toCurrency}/${this.state.amount}?format=json`)
+            axios.get(`https://api.ofx.com/PublicSite.ApiService/OFX/spotrate/Individual/${this.state.fromCurrency}/${this.state.toCurrency}/${this.state.amount}?format=json`)
                 .then(res => {
                     const rate = res.data.CustomerRate
                     const result = res.data.CustomerAmount
                     this.setState({
                         rate, result
                     })
-                    }
-                )
-                .catch(function (error) {
-                    // handle error
+                    this.props.history.push({ pathname: '/result', state: { rate, result, fromCurrency, toCurrency, amount } });
+                })
+                .catch(error => {
                     console.log(error);
                 });
-
-            
-              fetch(`https://api.ofx.com/PublicSite.ApiService/OFX/spotrate/Individual/${this.state.base}/${this.state.convertTo}/${this.state.amount}?format =json`)
-              .then(res => res.json())
-              .then(data => {
-                  const rate = data.CustomerRate
-                  const result = data.CustomerAmount
-                  this.setState({
-                      rate, result
-                  })
-              })
-          */
         }
     };
 
     render() {
         const { currencies, fromCurrency, amount, toCurrency } = this.state
-
         return (
             <div className="container">
-                <h1 className="header text-left pb-2" style={{borderBottom: `4px solid #92a8d1`}}>Quick Quote</h1>
-                <form class="form mt-3">
-                <div className="form-row text-left">
-                    <div className="form-group col-md-6">
-                        <label>First Name  <span style={{ color: "red" }}>*</span></label>
-                        <input type="text" className="form-control" placeholder="First Name" />
+                <h1 className="header text-left pb-2" style={{ borderBottom: `4px solid #92a8d1` }}>Quick Quote</h1>
+                <form className="form mt-3 text-left" onSubmit={this.getCurrencyData}>
+                    <div className="form-row text-left">
+                        <div className="form-group col-md-6">
+                            <label>First Name  <span style={{ color: "red" }}>*</span></label>
+                            <input type="text" className="form-control" placeholder="First Name" />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Last Name  <span style={{ color: "red" }}>*</span></label>
+                            <input type="text" className="form-control" placeholder="Last Name" />
+                        </div>
                     </div>
-                    <div className="form-group col-md-6">
-                        <label>Last Name  <span style={{ color: "red" }}>*</span></label>
-                        <input type="text" className="form-control" placeholder="Last Name" />
+                    <div className="form-group text-left">
+                        <label>Email</label>
+                        <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
                     </div>
-                </div>
-                <div className="form-group text-left">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
-                </div>
-                <span className="telephone mb-4">Telephone / Mobile</span>
-                <div className="form-row text-left">
-                    <div className="form-group col-md-2 pr-0">
-                        <select class="form-control">
-                            <option>+61</option>
-                            <option>+64</option>
-                            <option>+44</option>
-                            <option>+81</option>
-                        </select>
+                    <span className="telephone mb-4">Telephone / Mobile</span>
+                    <div className="form-row text-left">
+                        <div className="form-group col-md-2 pr-0">
+                            <select className="form-control">
+                                <option>+61</option>
+                                <option>+64</option>
+                                <option>+44</option>
+                                <option>+81</option>
+                            </select>
+                        </div>
+                        <div className="form-group col-md-10 pl-0">
+                            <input type="number" className="form-control" />
+                        </div>
                     </div>
-                    <div className="form-group col-md-10 pl-0">
-                        <input type="number" className="form-control" />
-                    </div>
-                </div>
-                <div className="form-row text-left">
-                    <div className="form-group col-md-6">
-                        <label >From Currency <span style={{ color: "red" }}>*</span></label>
-                        <select 
-                            className="form-control"
-                            name="fromCurrency"
-                            value={fromCurrency}
-                            onChange={this.handleFromCurrencySelector}
+                    <div className="form-row text-left">
+                        <div className="form-group col-md-6">
+                            <label >From Currency <span style={{ color: "red" }}>*</span></label>
+                            <select
+                                className="form-control"
+                                name="fromCurrency"
+                                value={fromCurrency}
+                                onChange={this.handleFromCurrencySelector}
                             >{currencies.map(currency => <option
                                 key={currency}
                                 value={currency}
-                                >{currency}
-                            </option>)}               
-                        </select>
-                    </div>
-                    <div className="form-group col-md-6">
-                        <label >To Currency <span style={{ color: "red" }}>*</span></label>
-                        <select 
-                            className="form-control"
-                            name="toCurrency"
-                            value={toCurrency}
-                            onChange={this.handleToCurrencySelector}
+                            >{currency}
+                            </option>)}
+                            </select>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label >To Currency <span style={{ color: "red" }}>*</span></label>
+                            <select
+                                className="form-control"
+                                name="toCurrency"
+                                value={toCurrency}
+                                onChange={this.handleToCurrencySelector}
                             >{currencies.map(currency => <option
                                 key={currency}
                                 value={currency}
-                                >{currency}
-                            </option>)} 
-                        </select>
+                            >{currency}
+                            </option>)}
+                            </select>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label>Amount <span style={{ color: "red" }}>*</span></label>
+                            <input
+                                type="number"
+                                value={amount}
+                                onChange={this.handleInputChange}
+                                className="form-control"
+                            />
+                        </div>
                     </div>
-                    <div className="form-group col-md-6">
-                        <label for="inputEmail4">Amount <span style={{ color: "red" }}>*</span></label>
-                        <CurrencyInput className="form-control" value={amount} onChangeEvent={this.handleInputChange} />
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary rounded-pill mt-3 px-5" width="100px">GET QUOTE</button>
-            </form>
+                    <button className="btn btn-primary rounded-pill mt-3 px-5" type="submit" value="submit" width="100px">GET QUOTE</button>
+                </form>
             </div>
-            
+
         );
     }
 }
 
 
-export default QuoteForm;
+export default withRouter(QuoteForm);
--- src/containers/QuoteResult/QuoteResult.js
+++ src/containers/QuoteResult/QuoteResult.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './QuoteResult.css';
 
-export default () => <div className="quote-result">
-    <h3>OFX Customer Rate</h3>
-    <h1>0.7618</h1>
-    <span>From</span>
-    <h2>AUD 25,000.00</h2>
-    <h3>To</h3>
-    <h2>USD 19,045.00</h2>
-    <button type="submit" className="btn btn-primary rounded-pill mt-3 px-5" width="100px">GET QUOTE</button>
-</div>
-    
+class QuoteResult extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: this.props.location.state.result,
+            rate: this.props.location.state.rate,
+            fromCurrency: this.props.location.state.fromCurrency,
+            toCurrency: this.props.location.state.toCurrency
+        }
+    }
+
+    handleClickEvent = () => {
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="header text-left pb-2" style={{ borderBottom: `4px solid #92a8d1` }}>Quick Quote</h1>
+                <div className="quote-result mt-3">
+                    <div className="content">
+                        <h4>OFX Customer Rate</h4>
+                        <h1 className="currency-rate text-center mb-3">{this.props.location.state.rate}</h1>
+                        <h4>From</h4>
+                        <h2 className="fromCurrency mb-3">{this.props.location.state.fromCurrency} <span className="currency">{this.props.location.state.amount}</span></h2>
+                        <h4>To</h4>
+                        <h2>{this.props.location.state.toCurrency} <span className="currency">{this.props.location.state.result}</span></h2>
+                        <button className="btn btn-primary rounded-pill mt-5 px-4" type="button" onClick={this.handleClickEvent} width="100px">START NEW QUOTE</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(QuoteResult);
